a907239340d1a1ed5095552841190c03206a82bc
Cassiano Rodrigues <cassiano.rodrigues1995@gmail.com>
2020-11-12 11:40:25 -0300

    Criado a estrutura das tabelas, cliente e lutador no arquivo principal.py

--- principal.py
+++ principal.py
@@ -1,8 +1,35 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import url_for 
+from flask import redirect 
+from flask_sqlalchemy import SQLAlchemy 
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://usuario:senha@localhost/ufc'
+db = SQLAlchemy(app)
+
+class ufc(db.model): 
+    __table__="cliente"
+    _id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    nome = db.Column(db.String(50))
+    telefone = db.Column(db.String)
+    email = db.Column(db.String(120))
+    valor_aposta = db.Column(db.Integer)
+    def __init__(self, nome, telefone, email, valor_aposta):
+    self.nome = nome
+    self.telefone = telefone
+    self.email = email
+    self.valor_aposta = valor_aposta
+
+    __table="lutadores"
+    _id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    nome = db.Column(db.String(50))
+    def __init__(self,nome):
+    self.nome = nome
+
+db.create_all()
 
 @app.route("/")
 @app.route("/index")
